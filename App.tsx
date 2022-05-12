36696a31f703d0dbe8ddcdfd3e2d90ca088e16ee
Lucas Baptista <lucas.baptista@interfocus.com.br>
2022-05-12 11:30:21 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { Poppins_400Regular, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
+import React from 'react';
+import { View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import Home from './src/screens/Home';
+import theme from './src/styles/theme';
+
+
+
+export default function App() {  
+  let [fontsLoaded] = useFonts ({
+    Poppins_700Bold,
+    Poppins_400Regular
+  });
+
+  if(!fontsLoaded){
+    return (
+      <View/>
+    )
+  }
+  return (
+    
+    <ThemeProvider theme={theme}>
+       <Home/>
+    </ThemeProvider>
+  
+  );
+}
+
